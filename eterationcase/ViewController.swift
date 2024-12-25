0c86e45de4d154295e7b54e73dde0a5345d98f42
ahmetbugraozcann <ahmet.ozcan@partners.getir.com>
2024-12-25 18:57:35 +0300

    Initial Commit

---
+++ eterationcase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  eterationcase
+//
+//  Created by Ahmet Buğra Özcan on 25.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
