4267308bc0fe6096d7b8bdf263f00c407b49c809
ahmetbugraozcann <ahmet.ozcan@partners.getir.com>
2024-12-26 11:11:21 +0300

    Add primary tabs

---
+++ eterationcase/Screens/FavoritesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesViewController.swift
+//  eterationcase
+//
+//  Created by Ahmet Buğra Özcan on 25.12.2024.
+//
+
+import UIKit
+
+// MARK: - FavoritesViewController
+class FavoritesViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "Favorites"
+    }
+}
